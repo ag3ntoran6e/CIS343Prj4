c8dffcf2b48ca5af80fa8925710efa8b4f26326b
Devin Brown <devinbrown7@gmail.com>
2016-03-17 13:25:10 -0400

    Added init, get, set, add, subtract, to_s

--- Matrix.rb
+++ Matrix.rb
@@ -20,19 +20,23 @@ class Matrix
   #     parameters rows or columns or val is not of type Fixnum
   #     value of rows or columns is <= 0
   def initialize(rows=5, columns=5, val=0)
-    result = Matrix.new
-    Matrix.rows = rows
-    Matrix.columns = columns
-    unless rows.kind_of? Fixnum) || (columns.kind_of? Fixnum) || (val.kind_of? Fixnum)
+
+    @rows = rows
+    @columns = columns
+    @val = val
+    @data = []
+
+    if (!@rows.kind_of? Fixnum) || (!@columns.kind_of? Fixnum) || (!@val.kind_of? Fixnum)
       raise ArgumentError.new("Please enter a number.")
     end
-    unless (rows <= 0) || (columns <= 0)
+
+    if (rows < 1) || (columns < 1)
       raise ArguementError.new("Please enter a valid Matrix size.")
     end
 
     for r in 0..rows
       for c in 0..columns
-        Matrix. = val
+        @data.push val
       end
     end
   end
@@ -43,7 +47,9 @@ class Matrix
   #     parameters i or j is not of type Fixnum
   #     value of i or j is outside the bounds of Matrix
   def get(i, j)
+    # TODO: error checking
 
+    return @data[i*@columns+j]
   end
 
   # method to set the value of matrix element at location (i,j) to value of parameter val
@@ -52,7 +58,9 @@ class Matrix
   #     parameters i or j or val is not of type Fixnum
   #     the value of i or j is outside the bounds of Matrix
   def set(i, j, val)
+    # TODO: error checking
 
+    @data[i*@columns + j] = val
   end
 
   # method that returns a new matrix object that is the sum of this and parameter matrices.
@@ -60,6 +68,23 @@ class Matrix
   # raise IncompatibleMatricesError exception if the matrices are not compatible for addition operation
   def add(m)
 
+    # check validitiy
+    if (m.class != self.class)
+      raise ArgumentError.new("Parameter must be a matrix")
+    end
+    if (m.rows != @rows || m.columns != @columns)
+      raise IncompatibleMatricesError.new("Matricies must be same dimensions")
+    end
+
+    newMatrix = Matrix.new(@rows,@columns,0)
+
+    for r in 0..@rows
+      for c in 0..@columns
+        newMatrix.set(r,c, self.get(r,c) + (m.get(r,c)))
+      end
+    end
+
+    return newMatrix
   end
 
   # method that returns a new matrix object that is the difference of this and parameter matrices
@@ -67,6 +92,23 @@ class Matrix
   # raise IncompatibleMatricesError exception if the matrices are not compatible for subtraction operation
   def subtract(m)
 
+    # check validitiy
+    if (m.class != self.class)
+      raise ArgumentError.new("Parameter must be a matrix")
+    end
+    if (m.rows != @rows || m.columns != @columns)
+      raise IncompatibleMatricesError.new("Matricies must be same dimensions")
+    end
+
+    newMatrix = Matrix.new(@rows,@columns,0)
+
+    for r in 0..@rows
+      for c in 0..@columns
+        newMatrix.set(r,c, self.get(r,c) - (m.get(r,c)))
+      end
+    end
+
+    return newMatrix
   end
 
   # method that returns a new matrix object that is a scalar multiple of source matrix object
@@ -132,7 +174,11 @@ class Matrix
 
   # method that returns a string representation of matrix data in table (row x col) format
   def to_s
-
+    string = ""
+    for r in 0..@rows
+      string += "#{@data[r..r+@columns]}\n"
+    end
+    return string
   end
 
   # method that for each element in the matrix yields with information
@@ -161,20 +207,26 @@ def main
   m3 = Matrix.new(4,5,30)
   m4 = Matrix.new(3,5,40)
 
+  puts "-----m1-----"
   puts(m1)
+  puts "-----m2-----"
   puts(m2)
+  puts "-----m3-----"
   puts(m3)
+  puts "-----m4-----"
   puts(m4)
-
+  puts "-----m1.add(m2)-----"
   puts(m1.add(m2))
-
+  puts "-----m1.subtract(m2)-----"
   puts(m1.subtract(m2))
-
+  puts "-----m1.multiply(m3)-----"
   puts(m1.multiply(m3))
-
+  puts "-----m2.scalarmult(5)-----"
   puts(m2.scalarmult(5))
-
+  puts "-----Matrix.identity(5)-----"
   puts(Matrix.identity(5))
+
+  puts "-----"
 
   puts(m1 + m2)
 
